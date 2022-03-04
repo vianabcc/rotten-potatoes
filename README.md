a7057f5abd283f2f63d46aa4f0b1f4acd47c6865
Vinicius Viana <vinicius@mindsight.com.br>
2022-03-04 11:58:13 -0300

    build(docker): create Dockerfile and docker-compose.yaml

--- README.md
+++ README.md
@@ -11,3 +11,13 @@ MONGODB_PORT => Posta de acesso ao MongoDB
 MONGODB_USERNAME => Usuário do MongoDB
 
 MONGODB_PASSWORD => Senha do MongoDB
+
+## Executando o projeto com docker
+
+Para executar este projeto utilizando `docker`, execute:
+
+```sh
+  docker-compose up
+```
+
+Para acessar a aplicação clique [aqui](http:localhost:5000)
--- src/config.py
+++ src/config.py
@@ -10,11 +10,14 @@ MONGODB_DB = os.getenv("MONGODB_DB", "admin")
 MONGODB_HOST = os.getenv("MONGODB_HOST", "localhost")
 MONGODB_PORT = int(os.getenv("MONGODB_PORT", "27017"))
 MONGODB_USERNAME = os.getenv("MONGODB_USERNAME", "mongouser")
-MONGODB_PASSWORD = os.getenv("MONGODB_PASSWORD", "mongopwd") 
+MONGODB_PASSWORD = os.getenv("MONGODB_PASSWORD", "mongopwd")
 
-def on_starting(server):    
 
-    CONNECTION_STRING = "mongodb://" + MONGODB_USERNAME + ":" + MONGODB_PASSWORD + "@" + MONGODB_HOST 
+def on_starting(server):
+
+    CONNECTION_STRING = (
+        "mongodb://" + MONGODB_USERNAME + ":" + MONGODB_PASSWORD + "@" + MONGODB_HOST
+    )
 
     client = MongoClient(CONNECTION_STRING)
 
@@ -22,22 +25,22 @@ def on_starting(server):
     collection = db["filme"]
 
     if not collection.find().count() > 0:
-        with open('movies.yaml') as arquivo:
+        with open("movies.yaml") as arquivo:
             documento = yaml.full_load(arquivo)
 
-            for item, doc in documento.items():       
+            for item, doc in documento.items():
                 for item_filme in doc:
 
-
-                    filme = { "titulo": item_filme['titulo'],
-                                "resumo": item_filme['resumo'],
-                                "duracao": item_filme['duracao'],
-                                "lancamento": item_filme['lancamento'],
-                                "categoria": item_filme['categoria'],
-                                "elenco": item_filme['elenco'],
-                                "slide": item_filme['slide'],
-                                "thumb": item_filme['thumb'] }
-
+                    filme = {
+                        "titulo": item_filme["titulo"],
+                        "resumo": item_filme["resumo"],
+                        "duracao": item_filme["duracao"],
+                        "lancamento": item_filme["lancamento"],
+                        "categoria": item_filme["categoria"],
+                        "elenco": item_filme["elenco"],
+                        "slide": item_filme["slide"],
+                        "thumb": item_filme["thumb"],
+                    }
 
                     # filme = Filme(titulo=item_filme['titulo'],
                     #             resumo=item_filme['resumo'],
@@ -47,7 +50,7 @@ def on_starting(server):
                     #             elenco=item_filme['elenco'],
                     #             slide=item_filme['slide'],
                     #             thumb=item_filme['thumb'])
-                    
+
                     collection.insert_one(filme)
-                    
-            arquivo.close() 
+
+            arquivo.close()
